a8e628da2b74d8b0a6c95a58172a1e85579f8788
ShubhamY90 <143524360+ShubhamY90@users.noreply.github.com>
2025-12-30 20:40:56 +0530

    Time: 7 ms (48.55%) | Memory: 20.2 MB (21.04%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& graph, vector<int>& visit, int n, bool& found, int i){
-        visit[i] = 1;
-        
-        for(int l : graph[i]){
-            if(visit[l] == 0){
-                dfs(graph, visit, n, found, l);
+    void dfs(vector<vector<int>>& graph, int i, vector<int>& visited, bool& found){
+        visited[i] = 1;
+        for(auto l : graph[i]){
+            if(visited[l] == 0){
+                dfs(graph, l, visited, found);
             }
-            if(visit[l] == 1){
+            if(visited[l] == 1){
                 found = true;
                 return;
             }
         }
-        visit[i] = 2;
-        
+        visited[i] = 2;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses;
         vector<vector<int>> graph(numCourses);
-        for(auto &p : prerequisites){
+        for(auto p : prerequisites){
             graph[p[1]].push_back(p[0]);
         }
-        vector<int> visit(n, 0);
-        for(int i = 0; i < n; i++){
-            if(!visit[i]){
-                bool found = false;
-                dfs(graph, visit, n, found, i);
+        vector<int> visited(numCourses);
+        for(int i = 0; i < numCourses; i++){
+            if(visited[i] == 0){
+                bool found  = false;
+                dfs(graph, i, visited, found);
                 if(found){
                     return false;
                 }
