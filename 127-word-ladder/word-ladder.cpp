b764d4a1128c16968fbc0db18948b30b5a139ce0
ShubhamY90 <143524360+ShubhamY90@users.noreply.github.com>
2026-01-02 22:06:33 +0530

    Time: 69 ms (50.56%) | Memory: 21.6 MB (32.07%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> s;
+        for(int i = 0; i < wordList.size(); i++){
+            s.insert(wordList[i]);
+        }
         queue<tuple<string, int>> q;
         q.push({beginWord, 1});
         int n = wordList.size();
@@ -11,20 +15,36 @@ public:
             if(curr == endWord){
                 return level;
             }
-            for(int i = 0; i < n; i++){
-                if(!visited[i]){
-                    int diff = 0;
-                    for(int j = 0; j < wordList[i].length(); j++){
-                        if(wordList[i][j] != curr[j]){
-                            diff++;
-                        }
+            //using hashmap
+            string temp = curr;
+            for(int i = 0; i < curr.length(); i++){
+                char ori = temp[i];
+                for(int j = 0; j < 26; j++){
+                    char t = 'a' + j;
+                    temp[i] = t;
+                    if(temp == curr) continue;
+                    if(s.count(temp)){
+                        q.push({temp, level + 1});
+                        s.erase(temp);
                     }
-                    if(diff == 1){
-                        q.push({wordList[i], level + 1});
-                        visited[i] = true;
-                    }
+                    temp[i] = ori;
                 }
             }
+            //brute force check
+            // for(int i = 0; i < n; i++){
+            //     if(!visited[i]){
+            //         int diff = 0;
+            //         for(int j = 0; j < wordList[i].length(); j++){
+            //             if(wordList[i][j] != curr[j]){
+            //                 diff++;
+            //             }
+            //         }
+            //         if(diff == 1){
+            //             q.push({wordList[i], level + 1});
+            //             visited[i] = true;
+            //         }
+            //     }
+            // }
         }
         return 0;
     }
