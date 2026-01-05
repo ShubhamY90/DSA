a92644e24372136db1d199e9af7ced8c4ea98fdc
ShubhamY90 <143524360+ShubhamY90@users.noreply.github.com>
2026-01-05 20:55:49 +0530

    Time: 1 ms (91.23%) | Memory: 42.1 MB (84.23%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,5 +1,44 @@
+class DSU{
+    vector<int> parent, size;
+public:
+
+    DSU(int n){
+        parent.resize(n);
+        size.resize(n, 1);
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+    }
+    int find(int x){
+        if(parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    void unionBS(int pu, int pv){
+        int u = find(pu);
+        int v = find(pv);
+        if(u == v){
+            return;
+        }
+        // if(size[u] > size[v]){
+        //     parent[v] = u;
+        //     size[u] += size[v];
+        // }
+        if(size[v] > size[u]){
+            parent[u] = v;
+            size[v] += size[u];
+        }
+        else{
+            parent[v] = u;
+            size[u] += size[v];
+        }
+        return;
+    }
+
+};
+
 class Solution {
 public:
+
     void dfs(vector<vector<int>>& graph, vector<bool>& visited, int i){
         visited[i] = true;
         for(auto g : graph[i]){
@@ -13,19 +52,34 @@ public:
         if(connections.size() < n - 1){
             return -1;
         }
-        vector<vector<int>> graph(n);
+        DSU d(n);
         for(int i = 0; i < connections.size(); i++){
-            graph[connections[i][0]].push_back(connections[i][1]);
-            graph[connections[i][1]].push_back(connections[i][0]);
+            d.unionBS(connections[i][0], connections[i][1]);
         }
-        vector<bool> visited(n, false);
-        int sol = 0;
-        for(int i = 0; i < graph.size(); i++){
-            if(!visited[i]){
-                sol++;
-                dfs(graph, visited, i);
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            //find(i);
+            if(d.find(i) == i){
+                ans++;
             }
         }
-        return sol - 1;
+        return ans - 1;
+        // if(connections.size() < n - 1){
+        //     return -1;
+        // }
+        // vector<vector<int>> graph(n);
+        // for(int i = 0; i < connections.size(); i++){
+        //     graph[connections[i][0]].push_back(connections[i][1]);
+        //     graph[connections[i][1]].push_back(connections[i][0]);
+        // }
+        // vector<bool> visited(n, false);
+        // int sol = 0;
+        // for(int i = 0; i < graph.size(); i++){
+        //     if(!visited[i]){
+        //         sol++;
+        //         dfs(graph, visited, i);
+        //     }
+        // }
+        // return sol - 1;
     }
 };
