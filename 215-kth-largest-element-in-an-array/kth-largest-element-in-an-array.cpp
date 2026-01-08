bb52d9a5836da8e599ce1fb5b0fb5e5004a1d2c1
ShubhamY90 <143524360+ShubhamY90@users.noreply.github.com>
2026-01-08 23:13:25 +0530

    Time: 38 ms (35.01%) | Memory: 64.7 MB (16.71%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -5,11 +5,14 @@ public:
         for(int i = 0; i < nums.size(); i++){
             pq.push(nums[i]);
         }
-        int temp = pq.top();
+        //int temp = pq.top();
         for(int i = 0; i < k; i++){
-            temp = pq.top();
+            //temp = pq.top();
+            if(i == k - 1){
+                return pq.top();
+            }
             pq.pop();
         }
-        return temp;
+        return 0;
     }
 };
