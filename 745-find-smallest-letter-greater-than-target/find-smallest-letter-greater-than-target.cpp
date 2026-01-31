fb3f531f53ac7ecb3023ca1bb98129192dc7db01
ShubhamY90 <143524360+ShubhamY90@users.noreply.github.com>
2026-01-31 22:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (62.61%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
-    char nextGreatestLetter(vector<char>& letters, char target) {
-        for(auto c : letters){
-            if(c > target){
-                return c;
+    char binary(vector<char>& letters, char target){
+        int l = 0;
+        int r = letters.size() - 1;
+        char ans = letters[0];
+        while(l <= r){
+            int mid = (l + r)/2;
+            if(letters[mid] > target){
+                ans = letters[mid];
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
             }
         }
-        return letters[0];
+        return ans;
+    }
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        // for(auto c : letters){
+        //     if(c > target){
+        //         return c;
+        //     }
+        // }
+        return binary(letters, target);
     }
 };
