df0e78a3b7ea1dc9947a902709b615ad71054eed
ShubhamY90 <143524360+ShubhamY90@users.noreply.github.com>
2026-02-01 21:32:45 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (48.24%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for(int i = 1; i < nums.size(); i++){
-            pq.push(nums[i]);
-        }
-        int ans = 0;
-        ans += nums[0];
-        for(int i = 0; i < 2; i++){
-            ans += pq.top();
-            pq.pop();
-        }
+        sort(nums.begin() + 1, nums.end());
+        int ans = nums[0] + nums[1] + nums[2];
         return ans;
     }
 };
