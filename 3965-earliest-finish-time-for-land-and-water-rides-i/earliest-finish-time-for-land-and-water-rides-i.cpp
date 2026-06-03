d77e40d5b6ccd3058345fd7460161d3994501475
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-06-03 22:03:51 +0530

    Time: 0 ms (100.00%) | Memory: 91.7 MB (85.66%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& startL, vector<int>& durL, vector<int>& startW, vector<int>& durW) {
-        int minL = 3000, minW = minL, res = minW;
-        int n = startL.size(), m = startW.size();
-
-        for (int i = 0; i < n; i++)
-            minL = min(minL, startL[i] + durL[i]);
-
-        for (int i = 0; i < m; i++) {
-            minW = min(minW, startW[i] + durW[i]);
-            res = min(res, max(minL, startW[i]) + durW[i]);
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int n = landStartTime.size();
+        int m = waterStartTime.size();
+        int minL = INT_MAX;
+        for(int i = 0; i < n; i++){
+            minL = min(minL, landStartTime[i] + landDuration[i]);
         }
-
-        for (int i = 0; i < n; i++)
-            res = min(res, max(minW, startL[i]) + durL[i]);
-
-        return res;
+        int minW = INT_MAX;
+        for(int i = 0; i < m; i++){
+            minW = min(minW, waterStartTime[i] + waterDuration[i]);
+        }
+        int ans1 = INT_MAX;
+        for(int i = 0; i < m; i++){
+            if(waterStartTime[i] >= minL){
+                ans1 = min(ans1, waterStartTime[i] + waterDuration[i]);
+            }
+            else{
+                ans1 = min(ans1, minL + waterDuration[i]);
+            }
+        }
+        int ans2 = INT_MAX;
+        for(int i = 0; i < n; i++){
+            if(landStartTime[i] >= minW){
+                ans2 = min(ans2, landStartTime[i] + landDuration[i]);
+            }
+            else{
+                ans1 = min(ans1, minW + landDuration[i]);
+            }
+        }
+        return min(ans1, ans2);
     }
 };
