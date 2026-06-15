fb935c2bcdfce3c485fb69f589e14d15e751fcb9
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-06-15 23:00:49 +0530

    Time: 54 ms (71.52%) | Memory: 76 MB (42.95%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int longestPalindromeSubseq(string s) {
-        //reverse string as s1 and then find common subsequence
-        vector<vector<int>> dp(s.length() + 1, vector<int>(s.length() + 1, 0));
-        string s1 = s;
-        reverse(s1.begin(), s1.end());
-        for(int i = 1; i <= s.length(); i++){
-            for(int j = 1; j <= s.length(); j++){
-                if(s[i - 1] == s1[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else{
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+    int helper(string& s, int i, int j, vector<vector<int>>& dp){
+        if(j < i) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s[i] == s[j] && i == j){
+            return dp[i][j] = 1 + helper(s, i + 1, j - 1, dp);
         }
-        return dp[s.length()][s.length()];
+        if(s[i] == s[j] && i != j){
+            return dp[i][j] = 2 + helper(s, i + 1, j - 1, dp);
+        }
+        return dp[i][j] = max(helper(s, i, j - 1, dp), helper(s, i + 1, j, dp));
+    }
+    int longestPalindromeSubseq(string s) {
+        vector<vector<int>> dp(s.length() + 1, vector<int>(s.length() + 1, -1));
+        return helper(s, 0, s.length() - 1, dp);
     }
 };
