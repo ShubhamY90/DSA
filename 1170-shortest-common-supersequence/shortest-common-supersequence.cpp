9a91addccec4c7a849399439a98029ff56b170e3
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-06-16 00:21:43 +0530

    Time: 27 ms (37.69%) | Memory: 26.8 MB (42.69%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,75 +1,66 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        vector<vector<int>> dp(str1.length() + 1, vector<int>(str2.length() + 1, 0));
-        vector<bool> s1(str1.length(), false);
-        vector<bool> s2(str2.length(), false);
-        for(int i = 1; i <= str1.length(); i++){
-            for(int j = 1; j <= str2.length(); j++){
-                if(str1[i - 1] == str2[j - 1]){
-                    
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+    int lcs(string& a, string& b, int i, int j,
+            vector<vector<int>>& dp) {
 
-                }
-                else{
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
+        if (i == a.size() || j == b.size())
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (a[i] == b[j])
+            return dp[i][j] = 1 + lcs(a, b, i + 1, j + 1, dp);
+
+        return dp[i][j] = max(
+            lcs(a, b, i + 1, j, dp),
+            lcs(a, b, i, j + 1, dp)
+        );
+    }
+
+    string shortestCommonSupersequence(string str1, string str2) {
         int n = str1.size();
         int m = str2.size();
-        int i = n, j = m;
 
-        while (i > 0 && j > 0) {
-            if (str1[i - 1] == str2[j - 1]) {
-                s1[i - 1] = true;
-                s2[j - 1] = true;
-                i--;
-                j--;
-            }
-            else if (dp[i - 1][j] >= dp[i][j - 1]) {
-                i--;
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        lcs(str1, str2, 0, 0, dp);
+
+        string ans;
+        int i = 0, j = 0;
+
+        while (i < n && j < m) {
+
+            if (str1[i] == str2[j]) {
+                ans += str1[i];
+                i++;
+                j++;
             }
             else {
-                j--;
+                int down = (i + 1 < n)
+                    ? dp[i + 1][j]
+                    : 0;
+
+                int right = (j + 1 < m)
+                    ? dp[i][j + 1]
+                    : 0;
+
+                if (down >= right) {
+                    ans += str1[i];
+                    i++;
+                } else {
+                    ans += str2[j];
+                    j++;
+                }
             }
         }
-        i = 0;
-        j = 0;
-        string ans;
-        while(i < str1.length() && j < str2.length()){
-            if(s1[i] && s2[j]){
-                ans += str1[i];
-                i++;
-                j++;
-                //cout << "1st" << endl;
-            }
-            else if(s1[i]){
-                ans += str2[j];
-                j++;
-                //cout << "2st" << endl;
-            }
-            else if(s2[j]){
-                ans += str1[i];
-                i++;
-                //cout << "3st" << endl;
-            }
-            else{
-                ans += str1[i];
-                ans += str2[j];
-                i++;
-                j++;
-                //cout << "4st" << endl;
-            }
-        }
-        while(i < str1.length()){
-            ans += str1[i];
-            i++;
-        }
-        while(j < str2.length()){
-            ans += str2[j];
-            j++;
-        }
+
+        while (i < n)
+            ans += str1[i++];
+
+        while (j < m)
+            ans += str2[j++];
+
         return ans;
     }
 };
