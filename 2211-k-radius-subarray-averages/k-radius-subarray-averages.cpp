b89854d23da7aad8c27fc4fa85d51cb2e208b8da
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-06-23 01:01:09 +0530

    Time: 0 ms (100.00%) | Memory: 134.3 MB (61.37%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -7,23 +7,20 @@ public:
             vector<int> temp(n, -1);
             return temp;
         }
-        vector<int> ans;
+        vector<int> ans(n, -1);
         long long sum = 0;
         for(int i = 0; i < ((2*k) + 1); i++){
             sum += nums[i];
         }
         //cout << "from: " << 0 << " to: " << ((2*k) + 1) << " sum: " << sum << endl;
-        ans.push_back((int)(sum/(long long)((k*2) + 1)));
+        ans[k] = ((int)(sum/(long long)((k*2) + 1)));
         for(int i = k + 1; i < n - k; i++){
             sum -= nums[i - k - 1];
             sum += nums[i + k];
             //cout << "from: " << i - k << " to: " << i + k << " sum: " << sum << endl;
-            ans.push_back((int)(sum/(long long)((k*2) + 1)));
+            ans[i] = ((int)(sum/(long long)((k*2) + 1)));
         }
-        for(int i = 0; i < k; i++){
-            ans.push_back(-1);
-            ans.insert(ans.begin(), -1);
-        }
+        
         return ans;
     }
 };
