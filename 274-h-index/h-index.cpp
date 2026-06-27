e47d97ffed8c1e9dc9a03c2f32a16eaf191bc4b7
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-06-28 02:13:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (63.86%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        sort(c.begin(), c.end());
+        int n = c.size();
+        for (int i = 0; i < n; i++) {
+            if (c[i] >= n - i)
+                return n - i;
+        }
+
+        return 0;
+    }
+};
