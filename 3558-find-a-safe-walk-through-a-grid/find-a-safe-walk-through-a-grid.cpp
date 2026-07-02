190fa5c91c0b2b36648884e8c8b740b73748dc61
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-07-02 12:46:32 +0530

    Time: 16 ms (67.95%) | Memory: 30.6 MB (79.20%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
+    // 1. Thought Dijkstra first
+    // 2. Thought 0/1 BFS after hint
     bool helper(vector<vector<int>>& grid, int health){
         int m = grid.size();
         int n = grid[0].size();
-        //priority_queue<pair<int, int>> pq;
+        priority_queue<pair<int, pair<int,int>>,vector<pair<int, pair<int,int>>>,greater<pair<int, pair<int,int>>>> pq;
         vector<vector<int>> dist(m, vector<int>(n, -1));
-        //pq.push({0, 0});
-        queue<pair<int, int>> q;
-        q.push({0, 0});
+        pq.push({grid[0][0], {0, 0}});
+        // queue<pair<int, int>> q;
+        //q.push({0, 0});
         dist[0][0] = grid[0][0];
         int dx[] = {0, 0, -1, 1};
         int dy[] = {-1, 1, 0, 0};
+
+        while(!pq.empty()){
+            auto [c, temp] = pq.top();
+            auto [i, j] = temp;
+            pq.pop();
+            if (c > dist[i][j]) continue;
+            for(int k = 0; k < 4; k++){
+                int nx = i + dx[k];
+                int ny = j + dy[k];
+                if(nx >= 0 && ny >= 0 && nx < m && ny < n){
+                    int nc = c + grid[nx][ny];
+                    if(dist[nx][ny] == -1 || nc < dist[nx][ny]){
+                        dist[nx][ny] = nc;
+                        pq.push({nc, {nx, ny}});
+                    }
+                }
+            }
+        }
+
+
+        /*
+        <----- 0/1 BFS ----->
+
         while(!q.empty()){
             auto [i, j] = q.front();
             q.pop();
@@ -25,7 +50,7 @@ public:
                     }
                 }
             }
-        }
+        } */
         return (dist[m - 1][n - 1] < health);
     }
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
