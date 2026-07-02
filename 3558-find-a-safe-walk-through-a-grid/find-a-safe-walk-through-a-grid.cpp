28c113a8c1bb59d525ea66ed9792f09738f150ec
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-07-02 12:36:18 +0530

    Time: 13 ms (76.58%) | Memory: 32.3 MB (53.16%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool helper(vector<vector<int>>& grid, int health){
+        int m = grid.size();
+        int n = grid[0].size();
+        //priority_queue<pair<int, int>> pq;
+        vector<vector<int>> dist(m, vector<int>(n, -1));
+        //pq.push({0, 0});
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        dist[0][0] = grid[0][0];
+        int dx[] = {0, 0, -1, 1};
+        int dy[] = {-1, 1, 0, 0};
+        while(!q.empty()){
+            auto [i, j] = q.front();
+            q.pop();
+            for(int k = 0; k < 4; k++){
+                int nx = i + dx[k];
+                int ny = j + dy[k];
+                if(nx >= 0 && ny >= 0 && nx < m && ny < n){
+                    int nc = dist[i][j] + grid[nx][ny];
+                    if(dist[nx][ny] == -1 || nc < dist[nx][ny]){
+                        dist[nx][ny] = nc;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+        }
+        return (dist[m - 1][n - 1] < health);
+    }
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        return helper(grid, health);
+    }
+};
