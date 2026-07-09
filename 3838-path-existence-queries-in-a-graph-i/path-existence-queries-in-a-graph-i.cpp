d1e49d9aa6da8bdc2f1642963f9fc2e604d3eeb7
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-07-09 17:15:40 +0530

    Time: 79 ms (16.71%) | Memory: 240.5 MB (22.74%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,6 +1,50 @@
+class DSU{
+public:
+    vector<int> parent;
+    DSU(int n){
+        parent.resize(n);
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+    }
+    int find(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    bool unite(int u, int v){
+        int pu = find(u);
+        int pv = find(v);
+
+        if(pu == pv) return false;
+
+        parent[pv] = pu;
+        return true;
+    }
+};
+
 class Solution {
 public:
+    vector<bool> helper(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries){
+        DSU dsu(n);
+        for(int i = 1; i < n; i++){
+            if(abs(nums[i] - nums[i - 1]) <= maxDiff){
+                dsu.unite(i, i - 1);
+            }
+        }
+        vector<bool> ans;
+        for(auto q : queries){
+            if(dsu.find(q[0]) == dsu.find(q[1])) ans.push_back(true);
+            else ans.push_back(false);
+        }
+        return ans;
+    }
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+
+        //using DSU for finding connected components
+        return helper(n, nums, maxDiff, queries);
+
+        //just checking where does the condition of diff breaks 
+        //same approach as DSU just simpler
         vector<pair<int, int>> arr(n);
         arr[0].first = nums[0];
         arr[0].second = 0;
