509d3b2e4bf25c815527a4cb9a922faf8a98c318
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-07-29 20:46:44 +0530

    Time: 8 ms (35.86%) | Memory: 17 MB (95.77%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
+typedef pair<int, pair<int, int>> pp;
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int minE = INT_MAX;
-        int maxE = INT_MIN;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                minE = min(minE, matrix[i][j]);
-                maxE = max(maxE, matrix[i][j]);
-            }
+        int n = matrix.size();
+        priority_queue<pp, vector<pp>, greater<pp>> pq;
+        for(int i = 0; i < n; i++){
+            pq.push({matrix[i][0], {i, 0}});
         }
-        int ans = -1;
-        while(minE <= maxE){
-            int mid = minE + (maxE - minE)/2;
-            if(count(matrix, mid, m, n) >= k){
-                ans = mid;
-                maxE = mid - 1;
-            }
-            else minE = mid + 1;
-        }
-        return ans;
-    }
-    int count(vector<vector<int>>& matrix, int num, int m, int n){
-        int cnt = 0;
-        for(int i = 0; i < m; i++){
-            if(matrix[i][n - 1] > num){
-                int j = n - 1;
-                int temp = n;
-                while(j >= 0 && matrix[i][j] > num){
-                    j--;
-                    temp--;
+        while(--k){
+            auto [temp1, temp2] = pq.top();
+            auto [i, j] = temp2;
+            pq.pop();
+
+            if(j < n - 1){
+                if(temp1 <= matrix[i][j + 1]){
+                    pq.push({matrix[i][j + 1], {i, j + 1}});
                 }
-                cnt += temp;
-                //break;
             }
-            else cnt += n;
         }
-        return cnt;
+        return pq.top().first;
     }
 };
