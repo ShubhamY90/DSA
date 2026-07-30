f6438c64c1c183cff88a8f737f83d23f6ebde340
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-07-30 19:39:32 +0530

    Time: 3 ms (15.08%) | Memory: 8.6 MB (30.10%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char, int> freq;
+        for(auto c : s){
+            freq[c]++;
+        }
+        priority_queue<pair<int, char>> pq;
+        for(auto it : freq){
+            pq.push({it.second, it.first});
+        }
+        string ans;
+        while(!pq.empty()){
+            vector<pair<int, char>> temp;
+            for(int i = 0; i < 2; i++){
+                if(!pq.empty()){
+                    auto [cF, curr] = pq.top();
+                    ans += curr;
+                    cF--;
+                    if(cF > 0) temp.push_back({cF, curr});
+                    pq.pop();
+                }
+                
+            }
+
+            for(auto it : temp){
+                pq.push(it);
+            }
+            temp.clear();
+        }
+        for(int i = 1; i < ans.length(); i++){
+            if(ans[i - 1] == ans[i]){
+                return "";
+            }
+        }
+        return ans;
+    }
+};
