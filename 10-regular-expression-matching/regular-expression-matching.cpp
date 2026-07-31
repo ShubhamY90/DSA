ff507dfa8cef2a1685911516182c68e80c1f9886
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-08-01 01:10:22 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (71.37%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
+    int dp[21][21];
     bool helper(string &s, string& p, int i, int j){
         if (j == p.length())
             return i == s.length();
 
+        if(dp[i][j] != -1) return dp[i][j];
         bool first = (i < s.length() && (s[i] == p[j] || p[j] == '.'));
 
         if(j + 1 < p.length() && p[j + 1] == '*'){
             //if x* then i am either skipping x* together or i am using x any no. of times (*) only if the curr chars match otherwise false
-            return (helper(s, p, i, j + 2) || (first && helper(s, p, i + 1, j)));
+            return dp[i][j] = (helper(s, p, i, j + 2) || (first && helper(s, p, i + 1, j)));
         }
         //normal char i am using 1, 1 of both as they are same
         //if not equal then i just return false as both must match completely
-        return first && helper(s, p, i + 1, j + 1);
+        return dp[i][j] = (first && helper(s, p, i + 1, j + 1));
     }
     bool isMatch(string s, string p) {
+        memset(dp, -1, sizeof(dp));
         return helper(s, p, 0, 0);
     }
 };
