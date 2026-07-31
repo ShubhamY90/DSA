acfcb1daaa23d057b65d418bc3bdbfa8950b2519
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-07-31 19:29:13 +0530

    Time: 93 ms (26.83%) | Memory: 48 MB (17.78%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> graph(n + 1);
-        for (auto &t : times) {
-            graph[t[0]].push_back({t[1], t[2]});
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for(auto it : times){
+            adj[it[0]].push_back({it[1], it[2]});
         }
-
-        vector<int> dist(n + 1, INT_MAX);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
+        vector<bool> vis(n + 1, false);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({0, k});
-        dist[k] = 0;
-
-        while (!pq.empty()) {
-            auto [cost, node] = pq.top();
+        vis[k] = true;
+        vector<int> dis(n + 1, INT_MAX);
+        dis[k] = 0;
+        while(!pq.empty()){
+            auto [wi, curr] = pq.top();
             pq.pop();
 
-            //if (cost > dist[node]) continue;
-
-            for (auto &[next, wt] : graph[node]) {
-                int newCost = cost + wt;
-                if (newCost < dist[next]) {
-                    dist[next] = newCost;
-                    pq.push({newCost, next});
+            if(dis[curr] < wi) continue;
+            
+            for(auto nex : adj[curr]){
+                int nnex = nex.first;
+                int wn = wi + nex.second;
+                if(wn < dis[nnex]){
+                    dis[nnex] = wn;
+                    pq.push({wn, nnex});
+                    vis[nnex] = true;
                 }
             }
         }
-
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == INT_MAX) return -1;
-            ans = max(ans, dist[i]);
+        int ans = INT_MIN;
+        for(int i = 1; i < n + 1; i++){
+            ans = max(ans, dis[i]);
         }
-
+        if(ans == INT_MAX) return -1;
         return ans;
     }
 };
