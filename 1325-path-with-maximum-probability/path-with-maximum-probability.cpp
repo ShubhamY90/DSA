1fd620cc00e95ec6326d46d28c23359fe8401df4
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-08-01 18:47:36 +0530

    Time: 56 ms (37.84%) | Memory: 70.8 MB (69.44%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        vector<vector<pair<double, int>>> graph(n);
+        vector<vector<pair<int, double>>> adj(n);
         for(int i = 0; i < edges.size(); i++){
-            graph[edges[i][0]].push_back({(-1)*log(succProb[i]), edges[i][1]});
-            graph[edges[i][1]].push_back({(-1)*log(succProb[i]), edges[i][0]});
+            adj[edges[i][0]].push_back({edges[i][1], succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0], succProb[i]});
         }
-        
-        priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> pq;
 
-        vector<double> dist(n, INT_MAX);
-        pq.push({0, start_node});
-        dist[start_node] = 0;
+        priority_queue<pair<double, int>> pq;
+        vector<double> dist(n, INT_MIN);
+        pq.push({1.0, start_node});
+        dist[start_node] = 1;
         while(!pq.empty()){
-            auto [cost, curr] = pq.top();
+            auto [wt, curr] = pq.top();
             pq.pop();
-            
-            for(auto ne : graph[curr]){
-                if(dist[ne.second] > cost + ne.first){
-                    dist[ne.second] = cost + ne.first;
-                    pq.push({dist[ne.second], ne.second});
+
+            //if(wt > dist[curr]) continue;
+
+            for(auto nex : adj[curr]){
+                double wn = nex.second;
+                int ne = nex.first;
+                if(dist[ne] < (wn * wt)){
+                    dist[ne] = (wn*wt);
+                    pq.push({dist[ne], ne});
                 }
             }
         }
-        if(dist[end_node] != INT_MAX) return exp(-dist[end_node]);
-        return 0;
+
+        if(dist[end_node]==INT_MIN) return 0;
+        return dist[end_node];
     }
 };
