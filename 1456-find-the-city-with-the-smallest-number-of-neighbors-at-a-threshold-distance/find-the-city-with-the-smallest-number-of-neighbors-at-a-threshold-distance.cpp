0d64d4173dc89aafc99d37b6f6204e9c6a5f762e
Shubham Yadav <143524360+ShubhamY90@users.noreply.github.com>
2026-08-01 19:46:39 +0530

    Time: 15 ms (57.23%) | Memory: 17 MB (49.16%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,61 +1,39 @@
 class Solution {
 public:
-    void dijkstra(vector<vector<int>>& times, vector<vector<pair<int,int>>>& graph, vector<int>& dist, int n, int k){
-        
-
-        //vector<int> dist(n + 1, INT_MAX);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
-        pq.push({0, k});
-        dist[k] = 0;
-
-        while (!pq.empty()) {
-            auto [cost, node] = pq.top();
-            pq.pop();
-
-            if (cost > dist[node]) continue;
-
-            for (auto &[next, wt] : graph[node]) {
-                int newCost = cost + wt;
-                if (newCost < dist[next]) {
-                    dist[next] = newCost;
-                    pq.push({newCost, next});
-                }
-            }
-        }
-        return;
-    }
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        //int ans = 0;
-        //unordered_map<int, vector<int>> map;
-        vector<vector<pair<int,int>>> graph(n);
-        for (auto &t : edges) {
-            graph[t[0]].push_back({t[1], t[2]});
-            graph[t[1]].push_back({t[0], t[2]});
+        vector<vector<int>> grid(n, (vector<int>(n, INT_MAX)));
+        for(auto e : edges){
+            grid[e[0]][e[1]] = e[2];
+            grid[e[1]][e[0]] = e[2];
         }
-        int ans = INT_MAX;
-        int m = INT_MAX;
+
         for(int i = 0; i < n; i++){
-            vector<int> dist(n, INT_MAX);
-            dijkstra(edges, graph, dist, n, i);
-            int temp = 0;
-            for(int j = 0; j < n; j++){
-                cout << i << " : " << j << " -> " << dist[j] << endl;
-                if(j == i) continue;
-                if(dist[j] <= distanceThreshold){
-                    temp++;
-                    //map[i].push_back(j);
-                    cout << j << endl;
+            grid[i][i] = 0;
+        }
+
+        for(int via = 0; via < n; via++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(grid[i][via] != INT_MAX && grid[via][j] != INT_MAX){
+                        grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
+                    }
                 }
             }
-            //cout << "temp : " << temp << " ans : " << ans << endl;
-            if(temp <= m){
-                //cout << "updating ans" << endl;
-                m = temp;
-                ans = i;
+        }
+
+        int minE = INT_MAX;
+        int minV = INT_MAX;
+        for(int i = n - 1; i >= 0; i--){
+            int currE = i;
+            int currV = 0;
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] <= distanceThreshold) currV++;
+            }
+            if(currV < minV){
+                minE = currE;
+                minV = currV;
             }
         }
-        
-        return ans;
+        return minE;
     }
 };
